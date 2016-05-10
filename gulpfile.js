492c2995d2bdf07f0114e7d596ea83eed8e8a73f
Yuliya <yuliya-krashanov@yandex.ua>
2016-05-10 18:01:16 +0300

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,31 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.sass('*.scss');
+
+
+    mix.styles([
+       /* 'bootstrap.css',
+        'fonts.css',
+        'layout.css',
+        'pop_up.css',
+        'responsive.css',
+        'app.css'*/
+    ], null, 'public/css');
+
+    mix.scripts([
+       /* 'bootstrap.min.js',
+        'typeahead.jquery.min.js',
+        'app.js',
+        'stripe.js',*/
+    ], null, 'public/js')
+        .scripts([
+          /*  'bootstrap.min.js',
+            'dataTables.bootstrap.min.js',*/
+        ], 'public/js/admin.js');
+
+    mix.version([
+        'public/css/all.css',
+        'public/js/all.js']);
+
 });
